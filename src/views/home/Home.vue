206d5fc9f2d0b195dd309c021e3c38db7a8a970c
Yu <546448821@qq.com>
2020-10-13 14:39:48 +0800

    11

---
+++ src/views/home/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物车</div>
+    </nav-bar>
+    <home-swiper :banners="banners"/>
+    </home-swiper>
+  </div>
+</template>
+
+<script>
+  import NavBar from 'components/common/navbar/NavBar'
+  import {GetHomeMultidata} from 'network/home'
+  import HomeSwiper from './childrenComps/HomeSwiper'
+
+  export default {
+    name: "Home",
+    components: {
+      NavBar,
+      HomeSwiper
+    },
+    data() {
+      return {
+        banners: null
+      }
+    },
+    created() {
+      GetHomeMultidata().then(res => {
+        this.banners = res.data.banner
+        this.recommand = res.data.recommand
+        console.log(res.data)
+      })
+    }
+  }
+</script>
+
+<style scoped>
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
+</style>
